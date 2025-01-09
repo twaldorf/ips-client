671b3acba54d44799c7dce4504fc73814807a515
twaldorf <trevor.automated@gmail.com>
2025-01-09 07:42:57 -0600

    complete basic login functionality

--- src/components/auth/CreateUser.tsx
+++ src/components/auth/CreateUser.tsx
@@ -11,7 +11,8 @@ export const CreateUser = (props) => {
 
   // Loading and error states
   const [ loading, setLoading ] = useState(false);
-  const [ error, setError ] = useState('')
+  const [ error, setError ] = useState('');
+  const [ success, setSuccess ] = useState(false);
 
   // Formstate manager
   const [ formState, setFormState ] = useState({
@@ -58,7 +59,10 @@ export const CreateUser = (props) => {
     });
     console.log(request);
     const response = await fetch(request, { mode: 'cors' });
-    if (response.ok) setLoading(false); else setError(JSON.stringify(response.body));
+    if (response.ok) {
+      setLoading(false);
+      setSuccess(true);
+    } else setError(JSON.stringify(response.body));
     console.log(response);
   };
 
@@ -68,6 +72,7 @@ export const CreateUser = (props) => {
       <Box style={containerStyle}>
         <h1>Create account</h1>
         <form action="" onSubmit={onSubmit}  style={signupForm}>
+          
           <label htmlFor="username">Username</label>
           <input type="text" name="username" onInput={handleInput} placeholder="Username" required minlength="6" value={formState.username}/>
           <label htmlFor="email">Email</label>
@@ -76,9 +81,12 @@ export const CreateUser = (props) => {
           <input type="password" name="password" onInput={handleInput} placeholder="Password" required minlength="8" value={formState.password}/>
           <label htmlFor="password2">Confirm Password</label>
           <input type="password" name="password2" onInput={handleInput} placeholder="Password again just to be sure" required minlength="8" value={formState.password2}/>
+
           { alerts.length > 0 && <h4 style={alertStyle}>{alerts}</h4> }
           { loading && <h4>Loading...</h4> }
           { error.length > 0 && <h4 style={alertStyle}>{error}</h4>}
+          { success && <h4>Success! Go to <a href="/login">login</a>.</h4> }
+
           <ActionButtonWrapper style={btn}>
             <input type="submit" value="Submit" style={buttonAction}>Submit</input>
           </ActionButtonWrapper>
--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,36 +1,84 @@
 import { Fragment } from "preact/jsx-runtime"
 import { apiUrl } from "../../config"
+import { Title } from "../Title"
+import { ActionButtonWrapper, Box } from "../ui-utilities/uiComponents"
+import { buttonAction } from "../../styles/buttons"
+import { useState } from "preact/hooks"
+import { route } from "preact-router"
 
 export const Login = ( props ) => {
-  const formState = {}
+  const [ formState, setFormState ] = useState({
+    username: '',
+    password: ''
+  });
+  const [ loading, setLoading ] = useState(false);
+  const [ error, setError ] = useState("");
 
   const handleInput = (event) => {
     const target = event.target;
-    formState[target.name] = target.value;
-  }
+    setFormState((prev) => { 
+      return {...formState, [target.name]: target.value };
+    });
+  };
 
   const onSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    setError('');
     const request:Request = new Request(
       `${apiUrl}/auth/login`, { 
         body: JSON.stringify(formState), 
         method: 'POST',
         headers: {
-          "Content-Type": "text/plain",
+          "Content-Type": "application/json",
         }
       } );
     console.log(request)
     const response = await fetch( request );
-    console.log(response)
+    if (response.ok) {
+      setLoading(false);
+      setError('Success!')
+      route('/');
+    } else {
+      const error_text = await response.json();
+      setLoading(false);
+      setError(error_text.error);
+    }
   }
 
   return (
-    <div>
-      <form action="" onSubmit={onSubmit}>
-        <input type="text" name="username" onInput={handleInput}/>
-        <input type="password" name="password" onInput={handleInput}/>
-        <input type="submit" value="login">Login</input>
-      </form>
-    </div>
+    <Fragment>
+      <Title></Title>
+      <Box style={containerStyle}>
+        <h1>Login</h1>
+        <p>Don't have an account yet? <a href="/newuser">Create one!</a></p>
+        <form action="" onSubmit={onSubmit} style={signupForm}>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" onInput={handleInput}/>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" onInput={handleInput}/>
+          { loading && <h4>Loading...</h4> }
+          { error.length > 0 && <h4>{error}</h4>}
+          <ActionButtonWrapper style={btn}>
+            <input type="submit" value="Login" style={buttonAction}>Login</input>
+          </ActionButtonWrapper>
+        </form>
+      </Box>
+    </Fragment>
   )
+}
+
+const btn = {
+  display: 'inline-grid'
+}
+
+const containerStyle = {
+  maxWidth: '400px',
+  margin: 'auto',
+}
+
+const signupForm = {
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
 }
